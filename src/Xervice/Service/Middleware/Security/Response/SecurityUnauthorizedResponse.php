aed64ebbee047e3214f1ddcd690abf01124904db
Mike Bertram <contact@mibexx.de>
2018-05-18 21:40:02 +0200

    * Status code if not authorized

--- src/Xervice/Service/Middleware/Security/Response/SecurityUnauthorizedResponse.php
+++ src/Xervice/Service/Middleware/Security/Response/SecurityUnauthorizedResponse.php
@@ -10,14 +10,18 @@ use Xervice\Service\Application\Response\ApiResponse;
 
 class SecurityUnauthorizedResponse extends ApiResponse implements SecurityUnauthorizedResponseInterface
 {
+    const STATUS_CODE = 403;
+
     /**
      * @param \Illuminate\Http\Request $request
      */
     public function setSecurityResponse(Request $request)
     {
         $message = new ApiAuthenticationFailedDataProvider();
-        $message->setStatus(403);
+        $message->setStatus(self::STATUS_CODE);
         $message->setMessage('Authentication failed');
+
+        $this->setStatusCode(self::STATUS_CODE);
 
         $this->setDataProvider($message);
     }
